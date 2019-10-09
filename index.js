032d91b5cd588b95ea7f90c3b16f9f39eecbd874
joseph townshend <joetownshend@mac.com>
2019-10-09 14:41:39 +0100

    setup: public folder with index.html - displays app title

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 const express = require('express')
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
-});
+app.listen(8000, () => console.log('Example app listening on port 8000!'));
+app.use(express.static('public'));
